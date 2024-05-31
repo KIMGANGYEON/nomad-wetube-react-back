33b38213972bee795013c34760b86298a32ddd7d
Kim Gang Yeon <rkddus3177@naver.com>
2024-05-31 22:18:57 +0900

    go

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,54 +1,29 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
-
-export const trending = (req, res) => res.send({ title: "Home", videos });
+export const home = (req, res) => res.send({ title: "Home" });
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.send({ title: "watch", video });
+  return res.send({ title: "watch" });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.send({ title: `Editing: ${video.title}`, video });
+  return res.send({});
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
 
   return res.status(200).json({ message: "Edit successful" });
 };
 
-export const getUpload = (req, res) => {};
+export const getUpload = (req, res) => {
+  res.status(200);
+  return res.send({ title: "Upload Page" });
+};
+
+export const postUpload = (req, res) => {
+  return res.status(200).json();
+};
 
 export const search = (req, res) => res.send("search");
-export const upload = (req, res) => res.send("upload");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
@@ -18,7 +18,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listenting on port http://localhost:${PORT} 🔥`);
-
-app.listen(4000, handleListening);
+export default app;
